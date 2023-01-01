76c2ef955f7667b45fe94a9c95dce40704f674c0
Aidan Rhoden <70079907+aidan-j-rhoden@users.noreply.github.com>
2023-01-01 15:16:49 -0600

    Begin work on day/night cycle

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,31 @@
 extends Spatial
 
+var round_time
+onready var timer = $game_timer
+onready var env = Environment
+var light = true
 
 func _ready():
-	var timer = $game_timer
 	timer.set_wait_time(gamestate.game_time())
-	print(timer.time_left)
+	round_time = gamestate.game_time()
 	timer.start()
+
+
+func _process(delta):
+	if timer.get_time_left() <= round_time/2:
+		cycle_light()
 
 
 func _on_game_timer_timeout():
 	gamestate.end_game()
+
+
+func cycle_light():
+	if light:
+		$directional_light.light_energy = 0
+		env.set_bg_energy(0.01)
+		env.set_ambient_light_energy(0)
+	else:
+		$directional_light.light_energy = 1
+		env.set_bg_energy(1)
+		env.set_ambient_light_energy(0)

151e38537f903672897393b51f0069aa05ef5eab
aidan-j-rhoden <aidan.j.rhoden@gmail.com>
2023-01-03 21:18:27 -0600

    Removed unnecessary stuff

--- scripts/physics/gibs.gd
+++ scripts/physics/gibs.gd
@@ -24,10 +24,7 @@ var avz
 
 
 func _ready():
-	slide = get_node("audio/slide")
 	hit = get_node("audio/hit")
-	roll = get_node("audio/roll")
-	whoosh = get_node("audio/whoosh")
 
 
 func _physics_process(delta):
@@ -43,47 +40,11 @@ func process_stuff():
 
 	var bodies = get_colliding_bodies()
 
-	if roll:
-		if bodies.size() > 0:
-			if !roll.playing:
-				if abs(avl) > 0.25:
-					roll.stream = ROLL_SLOW
-				if abs(avl) > 4:
-					roll.stream = ROLL_MEDIUM
-				if abs(avl) > 8:
-					roll.stream = ROLL_FAST
-				roll.play()
-		else:
-			roll.stop()
+	if bodies.size() > 0 and (prev_lvl - lvl) >= 0.25:
+		hit.stream = HIT[randi() % HIT.size()]
+		hit.play()
 
-		roll.unit_size = avl / 3
-
-	if slide:
-		if bodies.size() > 0 and lvl > 0.15:
-			
-			slide.pitch_scale = 1
-
-			if !slide.playing:
-				slide.stream = SLIDE
-				slide.play()
-
-		#	if lvl > 6:
-		#		var particles = particle_scn.instance()
-		#		globals.main.add_child(particles)
-		#		particles.global_transform.origin = global_transform.origin
-		#		particles.emitting = true
-		else:
-			slide.stop()
-
-		#slide.pitch_scale = clamp(lvl, 1, 1.1)
-		slide.unit_size = lvl
-
-	if hit:
-		if bodies.size() > 0 and (prev_lvl - lvl) >= 0.25:
-			hit.stream = HIT[randi() % HIT.size()]
-			hit.play()
-
-		hit.unit_size = lvl
+	hit.unit_size = lvl
 
 	# Set previous velocity
 	prev_lvl = lvl
